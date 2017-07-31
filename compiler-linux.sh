e392deec93e4f3f4e02d8d6e4966303ad83b1e8e
Martos <mauro95br@gmail.com>
2017-07-31 14:47:10 +0200

    Aggiunto script compiler-linux per linx(BETA)

---
+++ compiler-linux.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#Variabili
+scelta=0
+
+
+#Funzioni
+compile(){
+    gcc src/gameEngine.c src/win_utils.c main.c -o Halma
+    echo Soluzione compilata ...
+}
+
+update(){
+    git pull
+}
+
+#Main program
+echo ===================
+echo Easy Compiler Tools
+echo ===================
+echo
+echo 1. Compila soluzione
+echo 2. Aggiorna sorgente
+echo 0. Esci
+echo
+
+read scelta
+#echo $scelta
+
+case "$scelta" in
+    1) compile
+    ;;
+    2) update
+    ;;
+    0) exit
+    ;;
+    *) echo "error"
+    ;;
+esac
