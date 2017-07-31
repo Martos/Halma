50f3255900948fb026f736a72f638885552768c8
Martos <mauro95br@gmail.com>
2017-07-31 14:00:44 +0200

    Aggiunto compiler tool

---
+++ compiler.bat
@@ -0,0 +1,39 @@
+@echo off
+
+:MENU
+cls
+echo ===================
+echo Easy Compiler Tools
+echo ===================
+echo.
+
+echo 1. Compila soluzione
+echo 2. Aggiorna sorgente
+echo 0. Esci
+echo.
+
+SET /P M= "> " : 
+
+IF %M%==1 GOTO COMPILE
+IF %M%==2 GOTO UPDATE
+IF %M%==0 GOTO EOF
+GOTO ERROR_INPUT
+
+:COMPILE
+gcc .\src\gameEngine.c .\src\win_utils.c .\main.c -o Halma
+echo Soluzione compilata ...
+pause
+GOTO MENU
+
+:UPDATE
+git pull
+pause
+GOTO MENU
+
+:ERROR_INPUT
+echo Input errato ...
+pause
+GOTO MENU
+
+:EOF
+exit
