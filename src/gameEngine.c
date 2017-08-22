f9034e48cdecb58a69a13fa0386e8d09439e1dc9
Mauro.C <mauro95br@gmail.com>
2017-08-22 21:55:22 +0200

    ridotta la matrice a 8x8

--- src/gameEngine.c
+++ src/gameEngine.c
@@ -13,21 +13,16 @@ void initializeBoard(char playerIcon1, char playerIcon2){
     for (i = 0; i < ROWS; i++) {
         
         for(j = 0; j < COLUMNS; j++){
-
-            if(i == 0 && j < 6) halmaBoard[i][j] = playerIcon1;
-            else if(i == 1 && j < 5) halmaBoard[i][j] = playerIcon1;
-            else if(i == 2 && j < 4) halmaBoard[i][j] = playerIcon1;
-            else if(i == 3 && j < 3) halmaBoard[i][j] = playerIcon1;
-            else if(i == 4 && j < 2) halmaBoard[i][j] = playerIcon1;
-            else if(i == 5 && j < 1) halmaBoard[i][j] = playerIcon1;
+            if(i == 0 && j < 4) halmaBoard[i][j] = playerIcon1;
+            else if(i == 1 && j < 3) halmaBoard[i][j] = playerIcon1;
+            else if(i == 2 && j < 2) halmaBoard[i][j] = playerIcon1;
+            else if(i == 3 && j < 1) halmaBoard[i][j] = playerIcon1;
             else halmaBoard[i][j] = ' ';
 
-            if(i == 15 && j > 9) halmaBoard[i][j] = playerIcon2;
-            else if(i == 14 && j > 10) halmaBoard[i][j] = playerIcon2;
-            else if(i == 13 && j > 11) halmaBoard[i][j] = playerIcon2;
-            else if(i == 12 && j > 12) halmaBoard[i][j] = playerIcon2;
-            else if(i == 11 && j > 13) halmaBoard[i][j] = playerIcon2;
-            else if(i == 10 && j > 14) halmaBoard[i][j] = playerIcon2;
+            if(i == 7 && j > 3) halmaBoard[i][j] = playerIcon2;
+            else if(i == 6 && j > 4) halmaBoard[i][j] = playerIcon2;
+            else if(i == 5 && j > 5) halmaBoard[i][j] = playerIcon2;
+            else if(i == 4 && j > 6) halmaBoard[i][j] = playerIcon2;
         }
     }
 }
@@ -36,16 +31,18 @@ void printMatrix(){
     int i = 0,
         j = 0;
     
+    printf("\n    0  1  2  3  4  5  6  7 \n");
+    printf("\n");
     for(i = 0; i < ROWS; i++){
-
+        printf("%d  ", i);
         for(j = 0; j < COLUMNS; j++){
             printf("[%c]", halmaBoard[i][j]);
-            if(j == 15) printf("\n");
+            if(j == COLUMNS - 1) printf("\n");
         }
 
     }
 }
 
-void move(){
+void move(int x, int y, char giocatore){
 
 }
