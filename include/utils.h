8821484c7936305cce17e7acee885ff1ed430bca
Martos <mauro95br@gmail.com>
2017-08-25 10:12:26 +0200

    Aggiunti alcuni color ANSI

--- include/utils.h
+++ include/utils.h
@@ -2,6 +2,13 @@
     Nome : utils.h
     Desrizione : Libreria contenente funzioni di utilità multipiattaforma
 */
+
+#define ANSI_COLOR_BLUE "\033[22;34m"
+#define ANSI_COLOR_YELLOW "\033[22;33m"
+#define ANSI_COLOR_GREEN "\033[22;32m"
+#define ANSI_COLOR_RED "\033[22;31m"
+#define ANSI_COLOR_RESET "\033[0m"
+
 void consolePause();
 
 void screenClear();
